d9d298ec2d5b6e3f6b81499c4d0a5dea05492fc1
Charles-Chrismann <charles.chrismann@free.fr>
2023-02-16 20:17:04 +0100

    :tada: SETUP project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Menu from '$lib/Menu.svelte';
+    import Header from '$lib/Header.svelte';
+</script>
+
+<div class="App">
+    <Menu />
+    <Header />
+    <slot />
+    <aside>
+        <div class="recents"></div>
+        <div class="assets"></div>
+    </aside>
+</div>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+
+.App {
+  position: relative;
+  min-height: 100vh;
+  display: grid;
+  grid-template-areas: "header" "recents" "main";
+
+
+  @media screen and (min-width: 768px) {
+    margin-left: 0;
+    grid-template-areas: "header header" "main recents";
+  }
+  @media screen and (min-width: 1024px) {
+    margin-left: 360px;
+  }
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Menu from '$lib/Menu.svelte';
+    import Header from '$lib/Header.svelte';
+    let name = "SvelteKit"
+</script>
+
+
+
+<h1>Welcome to {name}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
